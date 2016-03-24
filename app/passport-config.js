2055718bea6567985dec428f0ba7eaaa99afc1e5
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-03-24 15:04:22 +0000

    Created passport/user creation route.

--- app/passport-config.js
+++ app/passport-config.js
@@ -5,4 +5,36 @@ var TwitStrat = require("passport-twitter"),
     
 module.exports = function(passport){
     
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    });
+    passport.deserializeUser(function(id, done){
+        User.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
+    
+    passport.use(new TwitStrat({
+        clientID: process.env.TWITCLI,
+        clientSecret: process.env.TWITSEC,
+        callbackURL: process.env.CALLBACK,
+        profileFields: ['id', 'displayName']
+    }, function(accessToken, refreshToken, profile, done){
+        User.findById(profile.id).then(function(err, user){
+           if(err){console.log(err);}
+           else{
+               if(user){
+                   return done(null, user);
+               }
+               else{
+                   var newUser = new User();
+                   newUser._id = profile.id;
+                   newUser.name = profile.displayName;
+                   
+                   newUser.save(function(err){if(err){console.log(err);} else{return done(null, newUser);}});
+               }
+           }
+        });
+    }));
+    
 };
