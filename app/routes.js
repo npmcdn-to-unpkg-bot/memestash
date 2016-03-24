6f1ef1d2234c49f8ce58bd94ec1d4a5048817935
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-03-24 15:10:11 +0000

    Added basic routes.

---
+++ app/routes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var p = process.cwd(),
+    backendJS = require('./controllers/backend.js');
+    
+module.exports = function(app, passport){
+        
+    function isLoggedIn(req, res, next){
+        req.session.redirect_url = req.originalUrl;
+        if(req.isAuthenticated()){
+            return next();
+        } else {
+            res.redirect('/login');
+        }
+    }
+    
+    app.route('/')
+    .get(function(req, res){
+        res.sendFile(p + "/public/index.html");
+    });
+    
+    app.route('/home')
+    .get(isLoggedIn, function(req, res){
+       res.sendFile(p + '/public/home.html');
+    });
+    
+    app.route('/auth/twitter')
+    .get(passport.authenticate("twitter"));
+    
+    app.route('/auth/twitter/callback')
+    .get(passport.authenticate("twitter", {
+        failureRedirect: '/',
+        successRedirect: '/home'
+    }));
+    
+}
